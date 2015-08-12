808e2fb9376e1fd9c71eec6f5ca110438b2eeb5f
Hamdi Bh <hamdibh@gmail.com>
2015-08-12 13:35:59 +0000

    Updating userprofile activity by adding new menu options

--- context-provider-idea/src/com/vlba/contextprovider/UserProfileActivity.java
+++ context-provider-idea/src/com/vlba/contextprovider/UserProfileActivity.java
@@ -2,8 +2,10 @@ package com.vlba.contextprovider;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.LinearLayout;
 
 import com.example.context_provider.R;
 
@@ -11,6 +13,10 @@ import com.example.context_provider.R;
  * Created by hamdi on 11/08/15.
  */
 public class UserProfileActivity extends Activity {
+
+    LinearLayout menuOptions;
+    LinearLayout option1;
+    LinearLayout option2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,14 +30,20 @@ public class UserProfileActivity extends Activity {
     }
 
     private void initializeApp() {
-
-        bindButtonsListener();
-    }
-
-    private void bindButtonsListener() {
-
+        bindLinearLayouts();
 
     }
+
+    private void bindLinearLayouts() {
+
+        menuOptions = (LinearLayout) findViewById(R.id.profileMenu);
+        option1 = (LinearLayout) findViewById(R.id.whitelist);
+        option2 = (LinearLayout) findViewById(R.id.whatToDo);
+
+
+    }
+
+
 
     private void initializeWindow() {
 
@@ -39,6 +51,35 @@ public class UserProfileActivity extends Activity {
 
         // Attaching UI to the activity
         setContentView(R.layout.userprofile);
+    }
+
+
+    @Override
+    public void onBackPressed()
+    {
+        if(menuOptions.getVisibility()==View.VISIBLE){
+            super.onBackPressed();
+        }else{
+            menuOptions.setVisibility(View.VISIBLE);
+            option1.setVisibility(View.GONE);
+            option2.setVisibility(View.GONE);
+        }
+
+    }
+    public  void  onClick(View v){
+
+        switch (v.getId()){
+            case R.id.btnUPoption1 : menuOptions.setVisibility(View.GONE);
+                option2.setVisibility(View.GONE);
+                option1.setVisibility(View.VISIBLE);
+                break;
+
+            case R.id.btnUPOption2 : menuOptions.setVisibility(View.GONE);
+                option2.setVisibility(View.VISIBLE);
+                option1.setVisibility(View.GONE);
+                break;
+
+        }
     }
 
 
