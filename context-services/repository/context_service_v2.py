c4a9efc7187c6150cd855354534d1f412d966bc0
Hamdi Bh <hamdibh@gmail.com>
2015-08-12 13:41:55 +0000

    Updating the server side by adding new routes (/push-profile) and changing the database type from sqlite to mongo db and testing the new routes

--- context-services/repository/context_service_v2.py
+++ context-services/repository/context_service_v2.py
@@ -6,14 +6,15 @@ from flask import Flask, jsonify, abort, request, make_response, url_for
 from flask.views import MethodView
 from flask.ext.restful import Api, Resource, reqparse, fields, marshal
 from flask.ext.httpauth import HTTPBasicAuth
+from dal_mongo import MongoDAL
 import json
-from dal_sqlite import SQLiteDAL
+
 
 HTTP_ROUTE_PREFIX = '/context/repository/api/v1.0/'
 
 app = Flask(__name__, static_url_path = "")
 api = Api(app)
-dal = SQLiteDAL()
+dal = MongoDAL()
 
 auth = HTTPBasicAuth()
 
@@ -29,8 +30,8 @@ def unauthorized():
     # return 403 instead of 401 to prevent browsers from displaying the default auth dialog
 
 service_fields = {
-    'name': fields.String,
-    'url': fields.String,
+    'name': fields.String
+,    'url': fields.String,
     'description': fields.String,    
     'note': fields.String
 }
@@ -71,8 +72,37 @@ class ContextPushesReciever(Resource):
         dal.insert_context(json.dumps(args))
         return { 'result': True }
 
-api.add_resource(ContextPushesReciever, HTTP_ROUTE_PREFIX + 'push-context', endpoint = 'push')
 
+class ProfileUpdateReciver(Resource):
+    """docstring for ProfileUpdateReciver"""
+    """decorators = [auth.login_required]"""
+    def __init__(self):
+        self.reqparse = reqparse.RequestParser()
+        self.reqparse.add_argument('test', type= str , location= 'json')
+        print 'halo'
+        
+
+        super(ProfileUpdateReciver, self).__init__()
+    def get(self):
+        return { 'task': marshal(services[1], service_fields) }
+
+    def put(self):
+        print 'oh yeah!'
+        return { 'status' :1 }
+        
+    def post(self):
+
+        args = self.reqparse.parse_args()
+        print args
+        
+        return { 'result': True }
+        
+        
+api.add_resource(ContextPushesReciever, HTTP_ROUTE_PREFIX + 'push-context', endpoint = 'push')
+api.add_resource(ProfileUpdateReciver, HTTP_ROUTE_PREFIX + 'push-profile', endpoint = 'update')
+
+
+"""  main  """
 if __name__ == '__main__':
     app.run(debug = True)
 
