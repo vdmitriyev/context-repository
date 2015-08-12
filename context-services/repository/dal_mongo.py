2b38037981b63073f9f586ff451542ad7b102588
Hamdi Bh <hamdibh@gmail.com>
2015-08-12 13:43:46 +0000

    Updating the DAL with the new columns

--- context-services/repository/dal_mongo.py
+++ context-services/repository/dal_mongo.py
@@ -6,6 +6,10 @@ client = MongoClient('localhost', 27017)
 DB_NAME = 'context-repository'
 CONTEXT_COL_NAME = 'user-context-col'
 PROFILE_COL_NAME = 'user-profile-col'
+SERVICE_COL_NAME='service_col'
+PERMISSION_COL_NAME='permission_col'
+
+
 
 class MongoDAL(object):
 
@@ -13,9 +17,22 @@ class MongoDAL(object):
 	def __init__(self):
 		self.db = client[DB_NAME]
 		self.context_col = self.db[CONTEXT_COL_NAME]
+		self.service_col = self.db[SERVICE_COL_NAME]
 		self.profile_col = self.db[PROFILE_COL_NAME]
-
+		self.permission_col = eslf.db[PERMISSION_COL_NAME]
+def
 	
+
+	 select_services(self):
+		print 'halo'
+
+		
+
+
+
+
+
+
 	def insert_context(self, context):
 		_id = self.context_col.insert(json.loads(context))
 		print 'Context document ID {0}'.format(_id)
